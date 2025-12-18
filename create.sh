e8a38f2230435a07bacc4fae8b9e2c5ec1484fd3
Ian Childress <howie2077@yahoo.com>
2025-12-17 20:56:14 -0500

    removed create.sh and pom.xml from repo

--- create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PGPASSWORD='Jaggar2006'
-BASEDIR=$(dirname $0)
-DATABASE=deep_builder
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
-createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
