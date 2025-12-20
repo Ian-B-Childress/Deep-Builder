ddaa0638f48a6c7c2125047adb568eb2198affa1
Ian Childress <howie2077@yahoo.com>
2025-12-19 20:16:20 -0500

    took 40 minutes to setup tailwind, worst experience of my life.

---
+++ frontend/my-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-red-500 text-white p-4">
+    <h1>please just work</h1>
+  </div>
+  <HelloWorld msg="create a user" />
+</template>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
